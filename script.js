cabc083e43d087b6bbfb107ab11e5b1b8534e2e8
Tets5441 <tetley640@gmail.com>
2019-04-03 22:05:16 +0100

    added color and hover to nav

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    const heading = document.createElement('h1');
+    heading.textContent = "PAW PRINT KENNELS";
+    
+    const logoText = document.querySelector('.logo-text');
+    logoText.appendChild(h1);
+
+  
+
+   
+
+
