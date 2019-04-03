12548eb0c1e5a4a51eafefd669811d7bf29dc97e
Tets5441 <tetley640@gmail.com>
2019-04-03 22:08:16 +0100

    added js for h1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
     const heading = document.createElement('h1');
     heading.textContent = "PAW PRINT KENNELS";
+    heading.className = "heading";
     
     const logoText = document.querySelector('.logo-text');
     logoText.appendChild(h1);
